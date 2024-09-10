b97eee7a9c18c987ed56d6046a200af1f28c7ed0
Raquel Bustamante <raquelbustamante.kb@gmail.com>
2024-09-09 22:29:24 -0300

    Adding score interface

--- snake_game/game.py
+++ snake_game/game.py
@@ -4,6 +4,11 @@ import sys
 from snake_classes import Snake, Food
 import time
 
+
+# inicar fonte
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 12)
+
 pygame.init()
 
 screen_size = (300, 400)
@@ -11,6 +16,8 @@ screen = pygame.display.set_mode(screen_size)
 
 # cronometro
 tempo = pygame.time.Clock()
+
+score = 0
 
 snake = Snake()
 food = Food()
@@ -40,12 +47,25 @@ while True:
     if snake.move(food_position):
         # comida devorada = True
         food.eaten = True
+        score += 1
 
     if snake.check_collision():
         # print("collided")
+        score_surface = my_font.render(f'Pontuação {score}', True, (255,255,255))
+        screen.blit(score_surface, (10,10))
+
+        you_lose = my_font.render(f'YOU LOSE!', True, (255,255,255))
+        screen.blit(you_lose, (100,180))
+        pygame.display.flip()
         time.sleep(2)
         pygame.quit()
         sys.exit()
+
+    # texto pontuacao
+    # aqui eu tive erro porque score era um inteiro e ao usar render() eu estava mudando para um objeto e causando erro
+    # por isso ajustei score para score_surface
+    score_surface = my_font.render(f'Pontuação {score}', True, (255,255,255))
+    screen.blit(score_surface, (10,10))
 
     # snake draw
     for pos in snake.body:
